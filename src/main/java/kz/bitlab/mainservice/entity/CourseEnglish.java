02b35de3e9af3a15bfe527952316c2d2f2549857
user <magzhankabi@gmail.com>
2025-04-02 11:50:10 +0500

    Подключение сущность

---
+++ src/main/java/kz/bitlab/mainservice/entity/CourseEnglish.java
@@ -0,0 +1,33 @@
+package kz.bitlab.mainservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "COURSE_ENGLISH")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CourseEnglish {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Column(name = "CREATED_TIME")
+    private LocalDateTime createdTime;
+
+    @Column(name = "UPDATED_TIME")
+    private LocalDateTime updatedTime;
+}
---
+++ src/main/java/kz/bitlab/mainservice/repository/CourseEnglishRepository.java
@@ -0,0 +1,4 @@
+package kz.bitlab.mainservice.repository;
+
+public interface CourseEnglishRepository {
+}
